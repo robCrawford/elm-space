850961c6918d1fc90cc777f85160cb1196389425
robCrawford <robCrawford@users.noreply.github.com>
2016-01-03 00:55:34 +0000

    Animation

--- Main.elm
+++ Main.elm
@@ -7,24 +7,27 @@ import Html.Attributes exposing (..)
 
 
 -- MODEL
+spriteXStart = -12544
+spriteXFrame = 256
+
 type alias State = {
     player: Player
   }
 
 type alias Player = {
+    visible: Bool,
     x: Int,
     y: Int,
-    fire: Bool,
-    visible: Bool
+    spriteX: Int
   }
 
 initialState: State
 initialState = {
     player = {
+      visible = False,
       x = 50,
       y = 50,
-      fire = False,
-      visible = False
+      spriteX = spriteXStart
     }
   }
 
@@ -36,11 +39,14 @@ model =
 -- UPDATE
 type UIEvents = MouseMove (Int, Int)
   | MouseButton Bool
+  | Frame Float
 
 uiEvents: Signal UIEvents
-uiEvents = Signal.merge
-    (Signal.map MouseMove Mouse.position)
-    (Signal.map MouseButton Mouse.isDown)
+uiEvents = Signal.mergeMany [
+    Signal.map MouseMove Mouse.position,
+    Signal.map MouseButton Mouse.isDown,
+    Signal.map Frame (fps 25)
+  ]
 
 update: UIEvents -> State -> State
 update uiEvents state =
@@ -48,15 +54,33 @@ update uiEvents state =
     MouseMove (x, y) ->
       { state | player = playerPos (x, y) state.player }
     MouseButton isDown ->
-      { state | player = playerFire isDown state.player }
+      { state | player = playerSpin isDown state.player }
+    Frame delta ->
+      { state | player = timeline delta state.player }
 
 playerPos: (Int, Int) -> Player -> Player
 playerPos (x, y) player = 
   { player | x = x, y = y, visible = True }
 
-playerFire: Bool -> Player -> Player
-playerFire isDown player = 
-  { player | fire = isDown }
+playerSpin: Bool -> Player -> Player
+playerSpin isDown player =
+    { player |
+        spriteX = 
+          if player.spriteX == spriteXStart && isDown == True then 
+            spriteXStart + spriteXFrame
+          else player.spriteX
+    }
+
+timeline: Float -> Player -> Player
+timeline delta player =
+    { player |
+        spriteX = 
+          if player.spriteX == 0 then 
+            spriteXStart
+          else if player.spriteX > spriteXStart then 
+            player.spriteX + spriteXFrame
+          else player.spriteX
+    }
 
   
 -- VIEW
@@ -71,15 +95,17 @@ view (winW, winH) state =
         , ("background", "#111")
         ]
     ] [ 
-      img [
-         src "x.png"
-        -- if player.fire then (src "x.gif") else (src "x.png")
-      , style
+      div [
+        style
           [ ("position", "absolute")
           , ("margin-left", "-128px")
           , ("margin-top", "-128px")
           , ("left", toString player.x ++ "px")
           , ("top", toString player.y ++ "px")
+          , ("background", "url(player.png) no-repeat " ++ (toString player.spriteX) ++ "px 0px")
+          , ("width", "256px")
+          , ("height", "256px")
+          , ("overflow", "hidden")
           , ("cursor", "none")
           , (if player.visible then ("display", "block") else ("display", "none"))
           ]
